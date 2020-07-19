b6ec6414a4599bb5c6942643338fc0cd7656945f
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-07-19 16:40:52 -0400

    idk

--- script.js
+++ script.js
@@ -5,17 +5,39 @@ const questionSection = $("#questionSection");
 const highScoreSection = $("#highScore");
 const highScoreButton = $("#highScoreButton");
 const clickAnswer = $(".clickAnswer");
+
+let highScoreForm = $(".highScoreForm");
+let initialsText = $("#initialsText");
+let initialForm = $("#initialForm");
+let scoreCount = $("#scoreCount");
+let initialsInput = $("#inputInitials");
+
+let correctAlert = $(".correctAlert");
+let incorrectAlert = $(".incorrectAlert");
+
+let optionA = $("#optionA");
+let optionB = $("#optionB");
+let optionC = $("#optionC");
+let optionD = $("#optionD");
+let questionText = $("#questionText");
 let timer = $("#timer");
 let countdownSeconds = 59;
-let score = 0;
+let score = countdownSeconds.text;
 let index = 0;
+let finalScore;
+
+let initialArr = [];
 
 $(document).ready(function () {
   // question and high score section should be hidden at start
   questionSection.hide();
   highScoreSection.hide();
 
+  correctAlert.hide();
+  incorrectAlert.hide();
+
   // questions to iterate through for quiz
+  // don't forget to credit the questions
   let quizObject = [
     {
       question:
@@ -67,20 +89,14 @@ $(document).ready(function () {
       countdownSeconds--;
       if (countdownSeconds < 0) {
         clearInterval(timerTime);
-        questionSection.hide();
-        highScoreSection.show(), 1000;
+        questionSection.hide(), 1000;
       }
     }, 1000);
+    //advance to high score
   }
 
   // when startBtn is clicked do:
   function showQuestions() {
-    let optionA = $("#optionA");
-    let optionB = $("#optionB");
-    let optionC = $("#optionC");
-    let optionD = $("#optionD");
-    let questionText = $("#questionText");
-
     $(questionText).text(quizObject[index].question);
     $(optionA).text(quizObject[index].answerA);
     $(optionB).text(quizObject[index].answerB);
@@ -88,27 +104,94 @@ $(document).ready(function () {
     $(optionD).text(quizObject[index].answerD);
   }
 
+  // function storeInitials() {
+  //   storeInitials.innerText = "";
+  // }
+
   startBtn.click(function () {
     startTimer();
     introSection.hide();
     questionSection.show(showQuestions);
   });
 
-  clickAnswer.click(function () {
-    index += 1;
+  clickAnswer.click(function (e) {
+    let btnValue = e.currentTarget.innerText;
     questionSection.show(showQuestions);
-    if (correctAnswer.click() === true) {
-      alert("that's correct!");
+
+    // correct answer & not last
+    if (
+      btnValue === quizObject[index].correctAnswer &&
+      quizObject.length - 1 !== index
+    ) {
+      // in
+      correctAlert.show();
+      setTimeout(function () {
+        correctAlert.hide();
+      }, 1000);
+      countdownSeconds += 5;
+      index += 1;
+      questionSection.show(showQuestions);
     }
+    // wrong answer & not last
+    else if (
+      btnValue !== quizObject[index].correctAnswer &&
+      quizObject.length - 1 !== index
+    ) {
+      //in
+      incorrectAlert.show();
+      setTimeout(function () {
+        incorrectAlert.hide();
+      }, 1000);
+      countdownSeconds -= 10;
+      index += 1;
+      questionSection.show(showQuestions);
+    } else if (
+      btnValue === quizObject[index].correctAnswer &&
+      quizObject.length - 1 === index
+    ) {
+      countdownSeconds += 5;
+      finalScore = countdownSeconds;
+      scoreCount.text(finalScore);
+      questionSection.hide();
+      highScoreSection.show();
+    } else {
+      countdownSeconds -= 10;
+      finalScore = countdownSeconds;
+      scoreCount.text(finalScore);
+      questionSection.hide();
+      highScoreSection.show();
+    }
+
+    return finalScore;
+  });
+
+  console.log(finalScore);
+
+  highScoreButton.click(function () {
+    questionSection.hide();
+    highScoreSection.show();
   });
 });
+// on event there has to be a value passed (how we acces data), the e can be anything, e stands for entire object of data that's passed on the event
+// can access/ store/ use
+// look through e value console.log
+// work on parameters next time
 
 // finishing
 // _____________
 //
-// [] have questions be right or wrong
-// [] wrong questions deduct 10 seconds / right add 5
+// [x] have questions be right or wrong
+// [x] wrong questions deduct 10 seconds / right add 5
 // []
 // [] would like to randomize questions
 // [] fix rendering order issue
 // []
+
+// return correct answer
+// known when btn clicked [x] / [x] what btn it is
+// on click get text value being passed in [x]
+// compare innertext to correct answer [x]
+// on correct answer +10 seconds to timer
+//
+// alert correct answer [x]
+// advance [x]
